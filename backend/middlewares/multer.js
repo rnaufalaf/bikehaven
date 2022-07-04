cc4f1bf6cbd5edd24494cbce3daab324804f7e80
Rakha Naufal <rnaufalaf@gmail.com>
2022-07-04 22:30:58 +0700

    deploy backend to heroku, fix multer image upload destination

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "../backend/images");
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "--" + file.originalname);
